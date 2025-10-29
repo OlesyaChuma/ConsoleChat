ccd2b6d4e82b24c5740c4f9e8535401e4be5a226
OlesyaChumachenko <olesya.chumachenko@dynamicsscientific.com>
2025-10-29 17:01:29 +0600

    Added UTF-8 console support for proper Russian text display

--- ConsoleChat/ConsoleChat/ConsoleChat.cpp
+++ ConsoleChat/ConsoleChat/ConsoleChat.cpp
@@ -2,11 +2,13 @@
 
 #include <iostream>
 #include "Chat.h"
+#include <clocale>          // для setlocale
 
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "");
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    system("chcp 65001 > nul");         // Устанавливаем кодировку UTF-8 в консоли
     Chat chat;
     chat.start();
     return 0;
